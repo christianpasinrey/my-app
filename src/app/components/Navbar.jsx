18f31343b955a909987f7d5c2e538392b3864097
Christian <christian.pasin.rey@gmail.com>
2023-10-02 11:43:00 +0200

    Base app with models folder with  a model base class and a user model example class, controllers folder and a userController example controller, and db conection

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,20 +2,20 @@
 import Link from 'next/link'
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid gap-3">
-                <Link href="/" className="navbar-brand">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light py-2 px-4">
+            <div className="flex sticky top-0 left-0 w-full z-90 gap-3">
+                <Link href="/" className="font-medium uppercase text-lg rounded-md px-2 dark:hover:bg-slate-700">
                 Home
                 </Link>
                {/*  <div className="collapse navbar-collapse" id="navbarNav">
                     <div className="navbar-nav"> */}
-                        <Link className="nav-link" href="/users/index">
+                        <Link className="font-medium uppercase text-lg rounded-md px-2 dark:hover:bg-slate-700" href="/users/index">
                             Users
                         </Link>
-                        <Link className="nav-link" href="/users/create">
+                        <Link className="font-medium uppercase text-lg rounded-md px-2 dark:hover:bg-slate-700" href="/users/create">
                             Users create
                         </Link>
-                        <Link className="nav-link" href="/users/show">
+                        <Link className="font-medium uppercase text-lg rounded-md px-2 dark:hover:bg-slate-700" href="/users/show">
                             Users show
                         </Link>
                     {/* </div>
--- src/database.js
+++ src/database.js
@@ -1,15 +1,17 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-    host: 'localhost',
-    database: 'react_test',
-    user: 'root',
-    password: ''
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
 });
 
 db.connect((err) => {
     if (err) {
         throw err;
-    }
-    console.log('Connected to database');
+    }  
 });
+console.log(
+    `Connected to ${process.env.DB_NAME} database on ${process.env.DB_HOST} as ${process.env.DB_USER}`
+);
